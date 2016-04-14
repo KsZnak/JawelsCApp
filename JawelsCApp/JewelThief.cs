70b0a6a44a954e22b327ed1703716e7d2d988f60
Kseniya <znakioma@mail.ru>
2016-04-15 02:33:27 +0300

    change2:33

---
+++ JawelsCApp/JewelThief.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JawelsCApp
+{
+    class JewelThief : Locksmith
+    {
+        private Jewels stolenJewels = null;
+        public void ReturnContents(Jewels safeContents, Owner owner)
+        {
+            stolenJewels = safeContents;
+            Console.WriteLine(" I'm stealing the contents!" + stolenJewels.Sparkle());
+        }
+    }
+    
+    
+}
